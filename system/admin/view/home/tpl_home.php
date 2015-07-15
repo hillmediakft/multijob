fc669c526bdc5e94e4e9ac4377e4be838c515be5
varnagy-attila <vucuka@gmail.com>
2015-07-15 16:48:12 +0200

    Módosítás

--- system/admin/view/home/tpl_home.php
+++ system/admin/view/home/tpl_home.php
@@ -1,137 +1,217 @@
 <!-- BEGIN CONTENT -->
-	<div class="page-content-wrapper">
-		<div class="page-content">
-				<!-- BEGIN PAGE HEADER-->
-					<!-- BEGIN PAGE TITLE & BREADCRUMB-->
-					<h3 class="page-title">
-						Admin <small>kezdőoldal</small>
-					</h3>
-					<div class="page-bar">
-						<ul class="page-breadcrumb">
-							<li>
-								<i class="fa fa-home"></i>
-								<a href="admin/home">Kezdőoldal</a> 
-								
-							</li>
-							
-						
-						</ul>
-					</div>
-					<!-- END PAGE TITLE & BREADCRUMB-->
-				<!-- END PAGE HEADER-->
-		
-				<!-- BEGIN PAGE CONTENT-->
-					<div class="row">
-						<!-- BEGIN DASHBOARD STATS -->
-						<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-							<div class="dashboard-stat blue-madison">
-								<div class="visual">
-									<i class="fa fa-comments"></i>
-								</div>
-								<div class="details">
-									<div class="number">
-										 1349
-									</div>
-									<div class="desc">
-										 New Feedbacks
-									</div>
-								</div>
-								<a href="#" class="more">
-								View more <i class="m-icon-swapright m-icon-white"></i>
-								</a>
-							</div>
-						</div>
-						<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-							<div class="dashboard-stat red-intense">
-								<div class="visual">
-									<i class="fa fa-bar-chart-o"></i>
-								</div>
-								<div class="details">
-									<div class="number">
-										 12,5M$
-									</div>
-									<div class="desc">
-										 Total Profit
-									</div>
-								</div>
-								<a href="#" class="more">
-								View more <i class="m-icon-swapright m-icon-white"></i>
-								</a>
-							</div>
-						</div>
-						<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-							<div class="dashboard-stat green-haze">
-								<div class="visual">
-									<i class="fa fa-shopping-cart"></i>
-								</div>
-								<div class="details">
-									<div class="number">
-										 549
-									</div>
-									<div class="desc">
-										 New Orders
-									</div>
-								</div>
-								<a href="#" class="more">
-								View more <i class="m-icon-swapright m-icon-white"></i>
-								</a>
-							</div>
-						</div>
-						<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-							<div class="dashboard-stat green-haze">
-								<div class="visual">
-									<i class="fa fa-shopping-cart"></i>
-								</div>
-								<div class="details">
-									<div class="number">
-										 549
-									</div>
-									<div class="desc">
-										 New Orders
-									</div>
-								</div>
-								<a href="#" class="more">
-								View more <i class="m-icon-swapright m-icon-white"></i>
-								</a>
-							</div>
-						</div>
-					</div><!-- END ROW--> 	
-					<!-- END DASHBOARD STATS -->
-					<div class="clearfix"></div>
-					
-					<div class="row ">
-						<div class="col-md-6 col-sm-6">
-							<div class="portlet box blue-steel">
-								<div class="portlet-title">
-									<div class="caption">
-										<i class="fa fa-bell-o"></i>Üzenetek
-									</div>
-									
-								</div>	
-								<div class="portlet-body">
-								Ez a portlet body
-								</div>
-							</div>
-						</div>	
+<div class="page-content-wrapper">
+    <div class="page-content">
+        <!-- BEGIN PAGE HEADER-->
+        <!-- BEGIN PAGE TITLE & BREADCRUMB-->
+        <h3 class="page-title">
+            Admin <small>kezdőoldal</small>
+        </h3>
+        <div class="page-bar">
+            <ul class="page-breadcrumb">
+                <li>
+                    <i class="fa fa-home"></i>
+                    <a href="admin/home">Kezdőoldal</a> 
+                </li>
+            </ul>
+        </div>
+        <!-- END PAGE TITLE & BREADCRUMB-->
+        <!-- END PAGE HEADER-->
 
-						<div class="col-md-6 col-sm-6">
-							<div class="portlet box green-haze">
-								<div class="portlet-title">
-									<div class="caption">
-										<i class="fa fa-check"></i>Tennivalók
-									</div>
-									
-								</div>	
-								<div class="portlet-body">
-								Ez a portlet body
-								</div>
-							</div>
-						</div>	
-					</div>
-			
-			
-				
-				
-		</div> <!-- END PAGE CONTAINER-->    
-	</div> <!-- END PAGE -->
+        <!-- BEGIN PAGE CONTENT-->
+        <div class="row">
+            <!-- BEGIN DASHBOARD STATS -->
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat blue-madison">
+                    <div class="visual">
+                        <i class="fa fa-users"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                        </div>
+                        <div class="desc">
+                            Felhasználók
+                        </div>
+                    </div>
+                    <a href="admin/users" class="more">
+                        Tovább <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat red-intense">
+                    <div class="visual">
+                        <i class="fa fa-file"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                        </div>
+                        <div class="desc">
+                            Szerkeszthető oldalak
+                        </div>
+                    </div>
+                    <a href="admin/pages" class="more">
+                        Tovább <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat green-haze">
+                    <div class="visual">
+                        <i class="fa fa-picture-o"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                        </div>
+                        <div class="desc">
+                            Képgaléria
+                        </div>
+                    </div>
+                    <a href="admin/photo-gallery" class="more">
+                        Tovább <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat purple-plum">
+                    <div class="visual">
+                        <i class="fa fa-cogs"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                        </div>
+                        <div class="desc">
+                            Beállítások
+                        </div>
+                    </div>
+                    <a href="admin/settings" class="more">
+                        Tovább <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div><!-- END ROW-->
+
+        
+        
+        <div class="row">
+            <!-- BEGIN DASHBOARD STATS -->
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat blue-hoki">
+                    <div class="visual">
+                        <i class="fa fa-desktop"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                        </div>
+                        <div class="desc">
+                            Kezdőoldali slider
+                        </div>
+                    </div>
+                    <a href="admin/slider" class="more">
+                        Tovább <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat green-seagreen">
+                    <div class="visual">
+                        <i class="fa fa-tags"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                        </div>
+                        <div class="desc">
+                            Termékek kezelése
+                        </div>
+                    </div>
+                    <a href="admin/products" class="more">
+                        Tovább <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat yellow-casablanca">
+                    <div class="visual">
+                        <i class="fa fa-folder-open-o"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                        </div>
+                        <div class="desc">
+                            Crew kezelése
+                        </div>
+                    </div>
+                    <a href="admin/crew-members" class="more">
+                        Tovább <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat blue-chambray">
+                    <div class="visual">
+                        <i class="fa fa-globe"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                        </div>
+                        <div class="desc">
+                            Nyelvi beállítások
+                        </div>
+                    </div>
+                    <a href="admin/languages" class="more">
+                        Tovább <i class="fa fa-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div><!-- END ROW-->         
+        
+        
+        <!-- END DASHBOARD STATS -->
+        <div class="clearfix"></div>
+<!--        <div class="row ">
+            <div class="col-md-6 col-sm-6">
+                <div class="portlet box blue-steel">
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <i class="fa fa-check"></i>Modulok
+                        </div>
+
+                    </div>	
+                    <div class="portlet-body">
+                        <h4>Oldalak</h4>
+                        <p>A weblap egyes oldalai szerkeszthetők, a szövegek szövegszerkesztő-szerű felületen módosíthatók. Módosítható a title és descriptipn elem is. Ezek módosításakor ügyeljen arra, hogy keresőoptimalizálás szempontjai szerint történjen a változtatás.</p>
+                        <h4>Egyéb tartalom</h4>
+                        <p>A jobb oldali sáv és a footer bemutatkozó szövege szerkeszthető.</p>
+
+                    </div>
+                </div>
+            </div>	
+
+            <div class="col-md-6 col-sm-6">
+                <div class="portlet box green-haze">
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <i class="fa fa-check"></i>Tennivalók
+                        </div>
+
+                    </div>	
+                    <div class="portlet-body">
+                        Ez a portlet body
+                    </div>
+                </div>
+            </div>	
+        </div>
+
+-->
+
+
+    </div> <!-- END PAGE CONTAINER-->    
+</div> <!-- END PAGE -->
 </div> 	<!-- END CONTAINER -->
