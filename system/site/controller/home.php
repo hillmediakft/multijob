76ccbab7a4ab99d11dd8ab33c0ba881fcb6dbff8
varnagy-attila <vucuka@gmail.com>
2015-09-11 21:40:28 +0200

    Slider.js és home.js linkelés

--- system/site/controller/home.php
+++ system/site/controller/home.php
@@ -1,30 +1,30 @@
-<?php 
+<?php
+
 class Home extends Controller {
 
-	function __construct()
-	{
-		parent::__construct();
+    function __construct() {
+        parent::__construct();
         Auth::handleExpire();
-		$this->loadModel('home_model');
-	}
+        $this->loadModel('home_model');
+    }
 
-
-	public function index()
-	{
+    public function index() {
+        
+        $this->view->js_link[] = $this->make_link('js', SITE_ASSETS, 'plugins/iosslider/_src/jquery.iosslider.min.js');
         $this->view->js_link[] = $this->make_link('js', SITE_ASSETS, 'pages/sidebar_search.js');
-		$this->view->js_link[] = $this->make_link('js', SITE_ASSETS, 'pages/common.js');
+        $this->view->js_link[] = $this->make_link('js', SITE_ASSETS, 'pages/common.js');
+        $this->view->js_link[] = $this->make_link('js', SITE_ASSETS, 'pages/home.js');
+        // lekérdezések
+        $this->view->settings = $this->home_model->get_settings();
+        $this->view->sliders = $this->home_model->slider_query();
+        // legújabb munkák
+        $this->view->latest_jobs = $this->home_model->jobs_query(9);
 
-	// lekérdezések
-		$this->view->settings = $this->home_model->get_settings(); 
-		$this->view->sliders = $this->home_model->slider_query(); 
-		// legújabb munkák
-		$this->view->latest_jobs = $this->home_model->jobs_query(9); 
-		
 //$this->view->debug(true); 
 
-		$this->view->render('home/tpl_home');
-	}
-	
-	
+        $this->view->render('home/tpl_home');
+    }
+
 }
+
 ?>
