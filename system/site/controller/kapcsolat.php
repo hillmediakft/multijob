90cff3451e043a4ee5a2e2bbd45d3ca77ed39c1e
varnagy-attila <vucuka@gmail.com>
2015-09-11 15:03:05 +0200

    Google Maps js link berakva

--- system/site/controller/kapcsolat.php
+++ system/site/controller/kapcsolat.php
@@ -1,29 +1,30 @@
-<?php 
+<?php
+
 class Kapcsolat extends Controller {
 
-	function __construct()
-	{
-		parent::__construct();
+    function __construct() {
+        parent::__construct();
         Auth::handleExpire();
-		$this->loadModel('kapcsolat_model');
-	}
+        $this->loadModel('kapcsolat_model');
+    }
 
-	public function index()
-	{
+    public function index() {
+        
+        $this->view->js_link[] = $this->make_link('js', 'http://maps.google.com/maps/api/js?sensor=true', '');
+        $this->view->js_link[] = $this->make_link('js', SITE_ASSETS, 'pages/kapcsolat.js');
 
-		$this->view->js_link[] = $this->make_link('js', SITE_ASSETS, 'pages/kapcsolat.js');
+        // alapbeállítások lekérdezése
+        $this->view->settings = $this->kapcsolat_model->get_settings();
+        // 3 legfrissebb munka
+        $this->view->latest_jobs = $this->kapcsolat_model->jobs_query(3);
+        // oldal tartalmának lekérdezése (paraméter a pages tábla page_id)
+        $this->view->content = $this->kapcsolat_model->get_page_data(13);
 
-		// alapbeállítások lekérdezése
-		$this->view->settings = $this->kapcsolat_model->get_settings();
-		// 3 legfrissebb munka
-		$this->view->latest_jobs = $this->kapcsolat_model->jobs_query(3);
-		// oldal tartalmának lekérdezése (paraméter a pages tábla page_id)
-		$this->view->content = $this->kapcsolat_model->get_page_data(13); 
-		
 //$this->view->debug(true); 	
 
-		$this->view->render('kapcsolat/tpl_kapcsolat');				
-	}
+        $this->view->render('kapcsolat/tpl_kapcsolat');
+    }
 
 }
+
 ?>
