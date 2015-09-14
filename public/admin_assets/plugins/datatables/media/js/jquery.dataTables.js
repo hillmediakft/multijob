49d16841edbd2f0db9d06e1af5ff1744c1afa9d4
varnagy-attila <vucuka@gmail.com>
2015-09-14 15:19:57 +0200

    datatables magyarítás

--- public/admin_assets/plugins/datatables/media/js/jquery.dataTables.js
+++ public/admin_assets/plugins/datatables/media/js/jquery.dataTables.js
@@ -10910,7 +10910,7 @@
 			 *      } );
 			 *    } );
 			 */
-			"sInfo": "Showing _START_ to _END_ of _TOTAL_ entries",
+			"sInfo": "_START_ / _END_ of _TOTAL_ elemből",
 	
 	
 			/**
@@ -10931,7 +10931,7 @@
 			 *      } );
 			 *    } );
 			 */
-			"sInfoEmpty": "Showing 0 to 0 of 0 entries",
+			"sInfoEmpty": "0 / 0 of 0 elemből",
 	
 	
 			/**
