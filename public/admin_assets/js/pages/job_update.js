2d38a3e66ff3cbdba5635d9ac13d70411720e90e
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2015-09-15 14:54:17 +0200

    js mods

--- public/admin_assets/js/pages/job_update.js
+++ public/admin_assets/js/pages/job_update.js
@@ -88,7 +88,9 @@ var updateJob = function () {
 				orientation: "left",
 				autoclose: true,
 				format:"yyyy-mm-dd",
-				language: "hu-HU"
+				language: "hu-HU",
+                                startDate: '0d',
+                                endDate: '+2m'
 			});
 			//$('body').removeClass("modal-open"); // fix bug when inline picker is used in modal
 		}
--- public/site_assets/pages/common_functions.js
+++ public/site_assets/pages/common_functions.js
@@ -1,17 +1,60 @@
 var common_functions = function () {
 
     var hideAlert = function () {
-		$('div.alert').delay(3000).fadeOut(500);						 		
-	}
+        $('div.alert').delay(3000).fadeOut(500);
+    }
+
+    var InitTabs = function () {
+        $('.tabs a').click(function (e) {
+            e.preventDefault();
+            $(this).tab('show');
+        });
+    }
+
+    var InitAccordion = function () {
+        $('.accordion').on('show', function (e) {
+            $(e.target).prev('.accordion-heading').find('.accordion-toggle').addClass('active');
+        });
+
+        $('.accordion').on('hide', function (e) {
+            $(this).find('.accordion-toggle').not($(e.target)).removeClass('active');
+        });
+    }
+
+    var InitChosen = function () {
+        $('.chosen-select').chosen({
+            disable_search_threshold: 10
+        });
+    }
+
+
+    var mobileMenu = function () {
+        $("#mobile_menu").mmenu({
+            // options
+            "offCanvas": {
+                "position": "right"
+            },
+            "extensions": ["widescreen", "border-full", "pageshadow"],
+        }, {
+            // configuration
+            classNames: {
+                vertical: "expand"
+            }
+        });
+    }
+
 
     return {
         //main function to initiate the module
         init: function () {
 
-           hideAlert();
-			
+            hideAlert();
+            InitTabs();
+            InitAccordion();
+            InitChosen();
+            mobileMenu();
         }
     };
 
-	
+
 }();
