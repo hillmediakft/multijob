e83e96b1096019d05a100e5351e43a63f183db6a
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2015-09-17 11:38:08 +0200

    kapcsolat js modositas

--- public/site_assets/pages/kapcsolat.js
+++ public/site_assets/pages/kapcsolat.js
@@ -1,143 +1,135 @@
 var kapcsolat = function () {
 
-    var koordinates = {
-       // default:
+    /*
+     * Google térkép objektumok
+     */
+    var googleMapsInit = function (office_lat, office_lng) {
+        
+        //var myLatlng = new google.maps.LatLng(47.498983, 19.058315);
+        var myLatlng = new google.maps.LatLng(office_lat, office_lng);
+                
+        var map_canvas = document.getElementById('map_canvas');
+
+        var map_options = {
+            scrollwheel: false,
+            center: myLatlng,
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: false,
+            zoomControl: true,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.LARGE,
+                position: google.maps.ControlPosition.LEFT_CENTER
+            },
+            panControl: true,
+            panControlOptions: {
+                position: google.maps.ControlPosition.LEFT_CENTER
+            },
+            scaleControl: false,
+            streetViewControl: true
+        };
+
+        var map = new google.maps.Map(map_canvas, map_options);
+        map.set('styles', [
+            {
+                stylers: [
+                    {saturation: -100}
+                ]
+            }, {
+                featureType: 'poi.business',
+                elementType: 'labels',
+                stylers: [
+                    {visibility: 'off'}
+                ]
+            }
+        ]);
+
+        var image = {
+            url: 'public/site_assets/image/marker.png',
+            size: new google.maps.Size(60, 62),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(91, 145)
+        };
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            icon: image
+        });         
+        
+    };
+
+    
+    /*
+     * A koordinátákat tároló objektum prototípusa (mint egy osztály)
+     */
+	var store_coordinates = function(latitude, longitude){
+		
+		this.latitude = latitude;
+		this.longitude = longitude;
+		
+		this.set_latitude = function($data){
+			this.latitude = $data;
+		}
+        this.set_longitude = function($data){
+			this.longitude = $data;
+		}
+		
+		this.get_latitude = function(){
+			return this.latitude;
+		}
+        this.get_longitude = function(){
+			return this.longitude;
+		}
+
+	};
+
+    /*
+     * A koordinátákat tároló objektum ebben a változóban fog tárolódni
+     */
+    var current_coordinates; 
+
+    /*
+     * Térkép adatok alapbeállítása
+     */
+    var set_default_coordinate = function() {
+        
+        var default_item = $('a[href="#collapse_1"]');
+        
+        var latitude = default_item.attr('data-latitude');
+        var longitude = default_item.attr('data-longitude');
+    
+        // létrehozzuk az objektumot (az objekum a current_coordinates változóban tárolódnak)
+        current_coordinates = new store_coordinates(latitude, longitude);
+        current_coordinates.set_latitude(latitude);
+        current_coordinates.set_longitude(longitude);
+
+        googleMapsInit(latitude, longitude);
     };
     
-    
-    var select_elemenet = function() {
-        var default_elem = $('a[href="#collapse_1"]');
-        
-        var lati = default_elem.attr('data-latitude');
-        var long = default_elem.attr('data-longitude');
-        
-        console.log(lati);
-        console.log(long);
-        
-        
-        //var lati = ;
-        //var long = ;
-    }
-    
-    
-    
-    var googleMapsInit = function () {
-console.log('googleMapsInit_meghívva');
-        var myLatlng = new google.maps.LatLng(47.498983, 19.058315);
-        var map_canvas = document.getElementById('map_canvas');
-        var map_options = {
-            scrollwheel: false,
-            center: myLatlng,
-            zoom: 15,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            mapTypeControl: false,
-            zoomControl: true,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.LARGE,
-                position: google.maps.ControlPosition.LEFT_CENTER
-            },
-            panControl: true,
-            panControlOptions: {
-                position: google.maps.ControlPosition.LEFT_CENTER
-            },
-            scaleControl: false,
-            streetViewControl: true
-        }
-        var map = new google.maps.Map(map_canvas, map_options);
-        map.set('styles', [
-            {
-                stylers: [
-                    {saturation: -100}
-                ]
-            }, {
-                featureType: 'poi.business',
-                elementType: 'labels',
-                stylers: [
-                    {visibility: 'off'}
-                ]
-            }
-        ]);
-
-        var image = {
-            url: 'public/site_assets/image/marker.png',
-            size: new google.maps.Size(60, 62),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(91, 145)
-        };
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            icon: image
-        });
-    }
-
-    var officeMap = function (office_lat, office_lng) {
-console.log('officeMap_meghívva');
-        var myLatlng = new google.maps.LatLng(office_lat, office_lng);
-        var map_canvas = document.getElementById('map_canvas');
-        var map_options = {
-            scrollwheel: false,
-            center: myLatlng,
-            zoom: 15,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            mapTypeControl: false,
-            zoomControl: true,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.LARGE,
-                position: google.maps.ControlPosition.LEFT_CENTER
-            },
-            panControl: true,
-            panControlOptions: {
-                position: google.maps.ControlPosition.LEFT_CENTER
-            },
-            scaleControl: false,
-            streetViewControl: true
-        }
-        var map = new google.maps.Map(map_canvas, map_options);
-        map.set('styles', [
-            {
-                stylers: [
-                    {saturation: -100}
-                ]
-            }, {
-                featureType: 'poi.business',
-                elementType: 'labels',
-                stylers: [
-                    {visibility: 'off'}
-                ]
-            }
-        ]);
-
-        var image = {
-            url: 'public/site_assets/image/marker.png',
-            size: new google.maps.Size(60, 62),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(91, 145)
-        };
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            icon: image
-        });
-    }
-    
-    var officeClick = function () { 
-        
+    /*
+     * Az oldal betöltődése után, új helyszín kiválasztásakor aktiválódik ez a metódus
+     */
+    var set_new_coordinate = function() {
         $('#accordion2').on('click', 'a[href*="#collapse_"]', function () {
-           console.log("kattintva");
-           officeMap(47.949289, 21.682472);  
-            
+            var this_item = $(this);
+            var latitude = this_item.attr('data-latitude');
+            var longitude = this_item.attr('data-longitude');    
+
+            // csak akkor töltjük újra a térképet, ha nem ugyanazok az előző koordináták, mint amire klikkelt a user (menüelem összezárásakor ugyanaz lesz)
+            if( (latitude != current_coordinates.get_latitude()) && longitude != current_coordinates.get_longitude() ){
+                googleMapsInit(latitude, longitude);
+                current_coordinates.set_latitude(latitude);
+                current_coordinates.set_longitude(longitude);
+            }            
         });
-    }   
+    };  
 
     return {
         //main function to initiate the module
         init: function () {
-
-            select_elemenet();
-            googleMapsInit();
-            officeClick();
-
+            set_default_coordinate();
+            set_new_coordinate();
         }
     };
 }();
