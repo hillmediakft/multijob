2412bfa405fcc9bc26c919f1d34acb81e71c1e61
varnagy-attila <vucuka@gmail.com>
2015-09-20 10:43:05 +0200

    CkEditor sablonok módosítás

--- public/admin_assets/plugins/ckeditor/plugins/templates/my_templates/mytemplates.js
+++ public/admin_assets/plugins/ckeditor/plugins/templates/my_templates/mytemplates.js
@@ -8,82 +8,40 @@ CKEDITOR.addTemplates("default",
 	
 	templates:
 		[
-            {
-				title:"cim - 2 oszlop - lablec",
-				//image:"template2.gif",
-				description:"Cím, szövegtest, lábszöveg",
-				html:
-                    '<div><h3>Cím helye</h3></div>' +
-                
-                    '<div class="row-fluid">' +
-                        '<div class="span6">' +
-                            '<div>Szöveg helye</div>' +
-                        '</div>' +
-                        '<div class="span6">' +
-                            '<div>Szöveg helye</div>' +
-                        '</div>' +
-                    '</div>' +
-
-                    '<div style="margin-top:30px; text-align:center;">' +
-                        '<div><p>Lorem ipsum dolor sit amet.</p></div>' +
-                    '</div>'
-
-			},
-            
-            {
-				title:"2 oszlop",
-				//image:"template2.gif",
-				description:"2 oszlop elem",
-				html:
-                    '<div class="row-fluid">' +
-                        '<div class="span6">' +
-                            '<div>Szöveg helye</div>' +
-                        '</div>' +
-                        '<div class="span6">' +
-                            '<div>Szöveg helye</div>' +
-                        '</div>' +
-                    '</div>'
-			},            
-            
-            {
-				title:"3 oszlop",
-				//image:"template2.gif",
-				description:"3 oszlop elem",
-				html:
-                    '<div class="row-fluid">' +
-                        '<div class="span4">' +
-                            '<div>Szöveg helye</div>' +
-                        '</div>' +
-                        '<div class="span4">' +
-                            '<div>Szöveg helye</div>' +
-                        '</div>' +
-                        '<div class="span4">' +
-                            '<div>Szöveg helye</div>' +
-                        '</div>' +
-                    '</div>'
-			}, 
-            
-            {
-				title:"4 oszlop",
-				//image:"template2.gif",
-				description:"4 oszlop elem",
-				html:
-                    '<div class="row-fluid">' +
-                        '<div class="span3">' +
-                            '<div>Szöveg helye</div>' +
-                        '</div>' +
-                        '<div class="span3">' +
-                            '<div>Szöveg helye</div>' +
-                        '</div>' +
-                        '<div class="span3">' +
-                            '<div>Szöveg helye</div>' +
-                        '</div>' +
-                        '<div class="span3">' +
-                            '<div>Szöveg helye</div>' +
-                        '</div>' +
-                    '</div>'
-			}
+			                      
+        {
+        title:"Szöveg dobozban",
+	image:"szoveg_dobozban.jpg",
+	description:"Szöveg dobozban",
+	html: '<div class="well">Nullam tincidunt gravida erat, vel faucibus ligula luctus a.&nbsp;</div>'
+        },
         
+        {
+        title:"Kiemelt szöveg",
+	image:"szoveg_kiemeles.jpg",
+	description:"Szöveg kiemelése nagyobb méretben",
+	html: '<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p></blockquote>'
+        },        
 
+{
+        title:"Lista elem",
+	image:"list.jpg",
+	description:"Pipával ellátott lista",
+	html: '<ul><li><i class="fa fa-check"></i>&nbsp;Lorem ipsus lures</li><li><i class="fa fa-check"></i>&nbsp;Lorem ipsus lures</li><li><i class="fa fa-check"></i>&nbsp;Lorem ipsus lures</li><li><i class="fa fa-check"></i>&nbsp;Lorem ipsus lures</li><li><i class="fa fa-check"></i>&nbsp;Lorem ipsus lures</li><li><i class="fa fa-check"></i>&nbsp;Lorem ipsus lures</li></ul>'
+        },          
+
+{
+        title:"Gomb linkkel",
+	image:"gomb_link.jpg",
+	description:"Linket tartalmazó további részletek gomb",
+	html: '<div class="margin-top-20 margin-bottom-20"><a href="#" class="btn btn-primary" target="_self">További részletek&nbsp;<i class="fa fa-arrow-right"></i></a></div>'
+        },          
+       
+{
+        title:"Link",
+	image:"link_arrow.jpg",
+	description:"Egyszerű link nyíllal",
+	html: '<p class="margin-bottom-15 margin-top-20"><a href="#"><i class="fa fa-arrow-right"></i> További információ</a></p>'
+        }         
 		]
 });
